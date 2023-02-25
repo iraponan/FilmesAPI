2ead3a4b63a7777497e0e28a828608359f0ae22c
Iraponan Aires de Souza Marinho <iraponan.aires@outlook.com>
2023-02-24 23:43:34 -0300

    .

--- FilmesAPI/Models/Filme.cs
+++ FilmesAPI/Models/Filme.cs
@@ -3,12 +3,13 @@
 namespace FilmesAPI.Models {
     public class Filme {
         [Required(ErrorMessage = "O título do filme é obrigatorio!")]
+        [StringLength(30)]
         public string Titulo { get; set; }
         [Required(ErrorMessage = "O gênero do filme é obrigatorio!")]
         [MaxLength(50, ErrorMessage = "O tamanho do gênero não pode exceder 50 caracteres.")]
         public string Genero { get; set; }
         [Required]
-        [Range(70, 600, ErrorMessage = "A duração deve ser entre 70 e 600 minutos.")]
+        [Range(1, 120, ErrorMessage = "A duração deve ser entre 70 e 600 minutos.")]
         public int Duracao { get; set; }
     }
 }
